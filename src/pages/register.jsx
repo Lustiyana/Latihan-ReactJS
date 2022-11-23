810d30d537dbc5ae15538acfa47444f544397536
Lustiyana <lustiyana1801@gmail.com>
2022-11-23 10:30:59 +0700

    Try to use Yup for register pages

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,34 +1,5 @@
-import React, { useEffect } from "react";
 import { useFormik } from "formik";
-
-const validate = (values) => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
-  }
-
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 15) {
-    errors.lastName = "Must be 15 characters or less";
-  }
-
-  if (!values.lastName) {
-    errors.noTelp = "Required";
-  } else if (values.noTelp.length > 13) {
-    errors.lastName = "Must be 13 characters or less";
-  }
-
-  return errors;
-};
+import * as Yup from "yup";
 
 const Register = () => {
   const formik = useFormik({
@@ -40,7 +11,25 @@ const Register = () => {
       password: "",
       validationPassword: "",
     },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      noTelp: Yup.string().max(15, "Must be 13").required("Required"),
+      password: Yup.string().required("Required"),
+      validationPassword: Yup.string().when("password", {
+        is: (values) => (values && values.length > 0 ? true : false),
+        then: Yup.string().oneOf(
+          [Yup.ref("password")],
+          "Both password need to be the same"
+        ),
+      }),
+    }),
+
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -96,6 +85,31 @@ const Register = () => {
         />
         {formik.touched.noTelp && formik.errors.noTelp ? (
           <div>{formik.errors.noTelp}</div>
+        ) : null}
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div>{formik.errors.password}</div>
+        ) : null}
+        <input
+          type="password"
+          id="vaidationPassword"
+          name="validationPassword"
+          placeholder="Confirm Password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.validationPassword}
+        />
+        {formik.touched.validationPassword &&
+        formik.errors.validationPassword ? (
+          <div>{formik.errors.validationPassword}</div>
         ) : null}
         <button type="submit">Submit</button>
       </form>
