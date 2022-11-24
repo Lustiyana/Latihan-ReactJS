29a7df0d11f4845bc360a75b1656e4584d82bccf
Lustiyana <lustiyana1801@gmail.com>
2022-11-24 07:39:02 +0700

    get api rick and morty

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [fetchData, setFetchData] = useState([]);
+
+  let api = `https://rickandmortyapi.com/api/character/?page=19`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      console.log(data);
+      setFetchData(data.results);
+    })();
+  }, []);
+
   const navigate = useNavigate();
   const handlerLogout = () => {
     alert("Want to Exit?");
@@ -10,7 +22,33 @@ const Home = () => {
   };
   return (
     <>
-      <h1 className="font-bold"> Home </h1>
+      <div>
+        {fetchData.map((data) => {
+          return (
+            <div key={data.id}>
+              {fetchData.map((data) => {
+                return (
+                  <>
+                    <div className="card max-w-sm rounded overflow-hidden border-2">
+                      <div className="mb-10">
+                        <img src={data.image} className="w-full" />
+                        <div className="px-6 py-4">
+                          <h5>{data.name}</h5>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+      {/* <h1 className="font-bold">
+        {fetchData.map((data) => {
+          return <p key={data.id}>{data.name}</p>;
+        })}
+      </h1> */}
       <button onClick={handlerLogout}>Logout</button>
     </>
   );
